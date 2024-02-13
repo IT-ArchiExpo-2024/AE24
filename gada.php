6656b9ce4af0b2b440579a89e5a96043a5a51f66
Ezekiel Farrell Yunanto <farrellyunanto@gmail.com>
2024-02-14 02:09:12 +0700

    tambah link early bird gada

--- gada.php
+++ gada.php
@@ -64,7 +64,7 @@
                     MORE INFO
                 </button>
                 <ul class="dropdown-menu dropdown-menu-secondary">
-                    <li><a class="dropdown-item" href="#">REGIST NOW</a></li>
+                    <li><a class="dropdown-item" href="https://docs.google.com/forms/d/e/1FAIpQLSfpkRZ90fOdEBzTscv6r8Fc-55mUfE_-CA3EEe9ES4UrvH7YA/viewform">REGIST NOW</a></li>
                     <li><a class="dropdown-item" href="#timeline" onclick="openCity('xGADA', 4)">TIMELINE & CONTACT</a></li>
                     <li><a class="dropdown-item" href="https://drive.google.com/file/d/1UDj5_6l62hNkRDHVY5tX1sNJbQcgyc-p/view" target="_blank">TERMS OF REFERENCE</a></li>
                 </ul>
